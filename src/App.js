8929ae4183c8d07dcf3e2f4a0574a02b954ebd9e
zul-adn <zbojetx@gmail.com>
2022-06-16 11:25:23 +0700

    init tailwind and main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  const 
+
+  const login = () => {
+    return(
+
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen p-0 md:px-96 lg:px-96  '>
+        <div className='h-screen bg-cyan-800'>
+
+        </div>
     </div>
   );
 }
