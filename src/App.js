6348102500f23d43e8293aca16ad21a24d946db0
zul-adn <zbojetx@gmail.com>
2022-06-17 22:20:36 +0700

    fixing login styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import io from "socket.io-client";
 
-let socket = io('http://101.50.0.208:5000')
-//let socket = io('http://localhost:5000')
+//let socket = io('http://101.50.0.208:5000')
+let ENDPOINT = 'http://localhost:5000';
+let socket = io('http://localhost:5000');
 
 let msgs = []
 
@@ -27,16 +28,38 @@ const App = () => {
   }, [])
 
 
-  const joinRoom = () => {
+  const joinRoom = async () => {
+
+    await fetch(`${ENDPOINT}/chat/${roomID}`)
+      .then(res => res.json())
+      .then(resJson => {
+        setMessages(resJson)
+      })
+
     console.log(roomID)
     socket.emit("join-room", {
       room: roomID
     })
   }
 
-  const login = () => {
-    setLoginStatus(!loginStatus)
-    joinRoom()
+  const login = async () => {
+
+    if(userName && roomID){
+      await fetch(`${ENDPOINT}/check/${userName}`)
+      .then(res => res.json())
+      .then(resJson => {
+        if (resJson > 0) {
+          alert("username exist")
+          return
+        }
+        setLoginStatus(!loginStatus)
+        joinRoom()
+      })
+    }else{
+      alert("Username and Room ID cannot be empty")
+    }
+
+   
   }
 
   const sendMessage = e => {
@@ -50,7 +73,7 @@ const App = () => {
       message
     })
     setMessages(newArr)
-    
+
     socket.emit("message", {
       nama: userName,
       room: roomID,
@@ -62,23 +85,29 @@ const App = () => {
 
   const loginScreen = () => {
     return (
-      <div className='h-screen flex justify-center items-center flex-col p-8'>
+      <div className='h-screen flex items-center flex-col p-8'>
+        <div className="text-lg mb-5 text-bold">
+          Join Chat Room
+        </div>
+        {/* <form onSubmit={login}> */}
         <input
-          className="w-full border border-black rounded py-2 px-5 my-2"
-          placeholder="Room ID"
-          onChange={e => setRoomID(e.target.value)}
-          required
-        >
-        </input>
-        <input
-          className="w-full border border-black rounded py-2 px-5 my-2"
+          className="w-full border bg-gray-100 rounded-md py-2 px-5 my-2"
           placeholder="Username"
           onChange={e => setUserName(e.target.value)}
           required
         >
         </input>
-        <div onClick={login} className="w-full border border-indigo-600 bg-indigo-600 text-white text-center rounded py-2 px-5 my-2">
-          Join Chat
+        <input
+          className="w-full border bg-gray-100 rounded-md py-2 px-5 my-2"
+          placeholder="Room ID"
+          onChange={e => setRoomID(e.target.value)}
+          required
+        >
+        </input>
+        <div className="absolute p-5 bg-white w-full bottom-0">
+          <div onClick={login} className=" w-full  bg-green-700 text-white text-center rounded-full py-2 px-5 my-2">
+            JOIN
+          </div>
         </div>
       </div>
     )
@@ -94,16 +123,16 @@ const App = () => {
       <div className='relative h-screen flex flex-col'>
 
         {/* Chat Header */}
-        <div className="p-5 bg-white w-full">
+        <div className="flex justify-center items-center p-5 bg-white w-full ">
           {roomID}
         </div>
 
         {/* Chat Body */}
-        <div className="flex flex-col p-5 bg-slate-100 w-full h-full">
+        <div className="flex flex-col p-5 bg-white w-full h-full">
           {messages && messages.map((msg, i) =>
-            <div key={i} className={`w-fit m-1 p-3 rounded  ${isMe(msg.nama) ? `self-end  bg-blue-500 text-white` : `self-start bg-white border text-black`}`}>
-                <div className={'text-xs'}>{msg.nama}</div>
-                {msg.message}
+            <div key={i} className={`w-fit m-1 p-3 rounded  ${isMe(msg.username) ? `self-end  bg-blue-500 text-white` : `self-start bg-white border text-black`}`}>
+              <div className={'text-xs'}>{msg.nama}</div>
+              {msg.message}
             </div>
           )}
         </div>
@@ -112,7 +141,7 @@ const App = () => {
         <div className="absolute p-5 bg-white w-full bottom-0">
           <form onSubmit={sendMessage}>
             <input
-              className="w-full bg-slate-100 rounded py-3 px-5 my-2"
+              className="w-full bg-slate-100 rounded-2xl py-3 px-5 my-2"
               placeholder="Text your message here"
               onChange={e => setMessage(e.target.value)}
               value={message}
