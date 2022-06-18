a5fd229983b0c70255134d868065593545383ecf
zul-adn <zbojetx@gmail.com>
2022-06-18 07:07:22 +0700

    complete styilng

--- src/App.js
+++ src/App.js
@@ -43,23 +43,23 @@ const App = () => {
   }
 
   const login = async () => {
+  
+    if (userName && roomID) {
+      await fetch(`${ENDPOINT}/check/${roomID}/${userName}`)
+        .then(res => res.json())
+        .then(resJson => {
+          alert("adadadad")
+          if (resJson > 0) {
+            alert("username exist")
+            return
+          }
 
-    if(userName && roomID){
-      await fetch(`${ENDPOINT}/check/${userName}`)
-      .then(res => res.json())
-      .then(resJson => {
-        if (resJson > 0) {
-          alert("username exist")
-          return
-        }
-        setLoginStatus(!loginStatus)
-        joinRoom()
-      })
-    }else{
+          setLoginStatus(!loginStatus)
+          joinRoom()
+        })
+    } else {
       alert("Username and Room ID cannot be empty")
     }
-
-   
   }
 
   const sendMessage = e => {
@@ -68,7 +68,7 @@ const App = () => {
     const newArr = [...messages]
 
     newArr.push({
-      nama: userName,
+      username: userName,
       room: roomID,
       message
     })
@@ -105,7 +105,7 @@ const App = () => {
         >
         </input>
         <div className="absolute p-5 bg-white w-full bottom-0">
-          <div onClick={login} className=" w-full  bg-green-700 text-white text-center rounded-full py-2 px-5 my-2">
+          <div onClick={login} className=" w-full bg-green-700 text-white text-center rounded-full py-2 px-5 my-2">
             JOIN
           </div>
         </div>
@@ -123,30 +123,35 @@ const App = () => {
       <div className='relative h-screen flex flex-col'>
 
         {/* Chat Header */}
-        <div className="flex justify-center items-center p-5 bg-white w-full ">
+        <div className="flex justify-center items-center p-1 bg-white w-full ">
           {roomID}
         </div>
 
         {/* Chat Body */}
         <div className="flex flex-col p-5 bg-white w-full h-full">
+          <div className="flex flex-col items-end pb-20 h-full overflow-y-scroll">
           {messages && messages.map((msg, i) =>
             <div key={i} className={`w-fit m-1 p-3 rounded  ${isMe(msg.username) ? `self-end  bg-blue-500 text-white` : `self-start bg-white border text-black`}`}>
-              <div className={'text-xs'}>{msg.nama}</div>
+              <div className={'text-xs'}>{msg.username}</div>
               {msg.message}
             </div>
           )}
+          </div>
         </div>
 
         {/* Chat Footer */}
-        <div className="absolute p-5 bg-white w-full bottom-0">
-          <form onSubmit={sendMessage}>
+        <div className="absolute p-1 bg-white w-full bottom-0">
+          <form className="flex w-full justify-between bg-slate-100 rounded-full py-2 px-2 my-2" onSubmit={sendMessage}>
             <input
-              className="w-full bg-slate-100 rounded-2xl py-3 px-5 my-2"
-              placeholder="Text your message here"
+              className="w-80 bg-slate-100"
+              placeholder="Message here ..."
               onChange={e => setMessage(e.target.value)}
               value={message}
-            >
-            </input>
+            />
+            <button type="submit" className="flex justify-center items-center rounded-full bg-green-700 h-8 p-2 aspect-square">
+              <img src={require('./assets/png/Vector.png')} />
+            </button>
+
           </form>
         </div>
 
